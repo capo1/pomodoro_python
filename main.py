68eb753db8dd49b6cbee0c36155466013cb95092
capo1 <capoeristas@wp.pl>
2019-09-24 08:26:41 +0200

    Update main.py
    end of loop in main thread

--- main.py
+++ main.py
@@ -1,79 +1,77 @@
+#! python3
 import time
 import sys
 
-
-Reg1 = None # global to get pause parameters
-
-toMinutes = 60 # multiply by 60 sec
-singleLoopTime = 10  # time in minutes for one task to multiply by $toMinutes
-countLoop=3 # After this no of Tasks set longBreak, starts at 0 :)
-shortBreak = 5  # time in minutes for shortBreak to multiply by $toMinutes
-longBreak = 5  # time in minutes for longBreak to multiply by $toMinutes
-
+REGISTRY = None # Global to get pause parameters
+toMinutes = 60 # Multiply by 60 sec, disabled for tests
+singleLoopTime = 10  # * toMinutes | Time in minutes for one task to multiply by $toMinutes
+countLoop = 3 # * toMinutes | After this no of Tasks set longBreak, starts at 0 :)
+shortBreak = 5  # * toMinutes | Time in minutes for shortBreak to multiply by $toMinutes
+longBreak = 15  # * toMinutes | Time in minutes for longBreak to multiply by $toMinutes
 
 def getFileWithTasks(fileName):
-  f = open(fileName, "r")
-  x = f.readlines()
-  f.close()
-  return x
+  taksFile = open(fileName, "r")
+  taskList = taksFile.readlines()
+  taksFile.close()
+  return taskList
 
 def clearLine():
   sys.stdout.write("\033[K")
 
-def setLoopForTask(string,lT): 
+def setLoopForTask(string,loop_time): 
 
   printOnConsole(string)
 
-  while lT >= 0:
-    printOnConsole(str('%02d:%02d' % (lT / 60, lT % 60)))
-    lT = lT - 1   
-    Reg1['time'] = lT
-  else: 
-    return
-
-def printOnConsole(Str):
-  print (Str+'\r', end="", flush=True)
-  time.sleep(1)
-
-def setBreak(string,sB):  
-  setLoopForTask(string,sB)
+  while loop_time >= 0:
+    printOnConsole(str('%02d:%02d' % (loop_time / 60, loop_time % 60)))
+    loop_time -= 1   
+    REGISTRY['time'] = loop_time
   return
 
-def loopForTask(fileName,sl,i):
-  o = i
-  global Reg1
-  Reg1={'time':0,'loopItem':0}
-  print("\nS-T-A-R-T    T-A-S-K-S")
-  mylist = getFileWithTasks(fileName)
-  breakTime = 0
-  listLenghth=len(mylist)
+def printOnConsole(string):
+  print (string+'\r', end="", flush=True)
+  time.sleep(1)
 
-  while (o < listLenghth):
+def setBreak(string,short_break):  
+  setLoopForTask(string,short_break)
+  return
+
+def loopForTask(fileName,single_loop_time,interation):
+  print("\n*** Pomodoro Method ***")
+
+  global REGISTRY
+  REGISTRY={'time':0,'loopItem':0}
+  taskList = getFileWithTasks(fileName)
+  breakTime = 0
+  listLenghth=len(taskList)
+
+  while (interation < listLenghth):
     clearLine()
-    startString="---\nStart task no #%d: %s\n\r" % (o+1,mylist[o])  
-    Reg1['loopItem'] = o
-    setLoopForTask(startString, sl)   
+    startString="---\nStart task no #%d: %s\n\r" % (interation+1,taskList[interation])  
+    REGISTRY['loopItem'] = interation
+    setLoopForTask(startString, single_loop_time)   
     
-    if(o % countLoop == 0 and o != 0 ):   
+    if(interation % countLoop == 0 and interation != 0 ):   
       breakTime=longBreak      
     else:
       breakTime=shortBreak  
 
     endString="\rYou have break for: %d:%02d minutes  \r\n"  %  (breakTime / 60, breakTime % 60)
     setBreak(endString,breakTime)
-    o += 1
-    sl = singleLoopTime
+    interation += 1
+    single_loop_time = singleLoopTime
 
+print("\nTo stop the script execution type <CTRL+C>")
 
-print("To stop the script execution type CTRL-C")
-
-while 1:
-  sl = Reg1['time'] if Reg1 else singleLoopTime
-  i =  Reg1['loopItem'] if Reg1 else 1
+while True:
+  single_loop_time = REGISTRY['time'] if REGISTRY else singleLoopTime
+  interation =  REGISTRY['loopItem'] if REGISTRY else 0
   
   try:
-    loopForTask("tasks.txt",sl,i)
+    loopForTask("tasks.txt",single_loop_time,interation)
+    clearLine()
+    break
   except KeyboardInterrupt:
-    resume = input('\nIf you want to resume type r\nor any other key to stop and exit:')
+    resume = input('\nIf you want to resume type <r> and press <Enter>\nor any other key to stop and exit script\ntype:')
     if resume != 'r':
       break
